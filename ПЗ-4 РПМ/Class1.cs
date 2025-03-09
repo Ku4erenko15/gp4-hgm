5a21ce89a651b1b255ef01c77b809aee62993dd0
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2025-03-09 19:15:14 +0200

    Add files via upload

---
+++ ПЗ-4 РПМ/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ПЗ_4_РПМ
+{
+    internal class Build
+    {
+        public string Address { get; set; }
+        public string Type { get; set; }
+        public int Floors { get; set; }
+        public int Apartments { get; set; }
+        public int YearsInUse { get; set; }
+        public int YearsToMajorRepair => 25 - (YearsInUse % 25);
+
+        public Build(string address, string type, int floors, int apartments, int yearsInUse)
+        {
+            Address = address;
+            Type = type;
+            Floors = floors;
+            Apartments = apartments;
+            YearsInUse = yearsInUse;
+        }
+    }
+}
---
+++ ПЗ-4 РПМ/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ПЗ_4_РПМ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Build> buildings = new List<Build>
+        {
+            new Build("ул. Ленина, 1", "Жилой", 5, 20, 60),
+            new Build("ул. Пушкина, 5", "Офисный", 10, 50, 45),
+            new Build("пр. Победы, 10", "Жилой", 9, 100, 80),
+            new Build("ул. Садовая, 7", "Коммерческий", 3, 10, 30)
+        };
+
+            var oldBuildings = buildings.Where(b => b.YearsInUse > 50);
+
+            Console.WriteLine("Здания со сроком эксплуатации более 50 лет:");
+            foreach (var building in oldBuildings)
+            {
+             Console.WriteLine($"Адрес: {building.Address}, Тип: {building.Type}, Этажей: {building.Floors}, " +
+                                  $"Квартир: {building.Apartments}, Срок эксплуатации: {building.YearsInUse} лет, " +
+                                  $"Срок до капитального ремонта: {building.YearsToMajorRepair} лет");
+            }
+        }
+    }
+}
