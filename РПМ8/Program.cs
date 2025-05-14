5226a114d012f6f06b993d623f16d96b711ba595
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2025-05-14 20:59:02 +0300

    Add files via upload

---
+++ РПМ8/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace РПМ8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ввод цифры D
+            Console.Write("Введите цифру D (от 0 до 9): ");
+            int D = int.Parse(Console.ReadLine());
+
+            // Ввод последовательности A
+            Console.WriteLine("Введите элементы последовательности A через пробел:");
+            string[] input = Console.ReadLine().Split(' ');
+            int[] A = Array.ConvertAll(input, int.Parse);
+
+            // Поиск первого подходящего элемента
+            foreach (int num in A)
+            {
+                if (num > 0 && num % 10 == D)
+                {
+                    Console.WriteLine("Результат: " + num);
+                    return;
+                }
+            }
+
+            // Если не найдено
+            Console.WriteLine("Результат: 0");
+
+        }
+    }
+}
